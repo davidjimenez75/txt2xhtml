6149eeb045d6b8647ecf412dddcc5b67de42d29f
David Jimenez <davidjimenez75@gmail.com>
2020-02-19 12:17:05 +0100

    Full Sigil EPub3 compatibility.

--- config.php
+++ config.php
@@ -8,6 +8,7 @@ $xhtml_header='<?xml version="1.0" encoding="utf-8"?>
 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops">
 <head>
   <title></title>
+  <link rel="stylesheet" href="../Styles/style.css" type="text/css"/>
 </head>
 
 <body>
